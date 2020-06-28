97af19c835751ba9dde2e82893e97e5d349f5b3a
Arun S Jois <arun.joiss@gmail.com>
2020-06-28 14:36:42 +0530

    Added javafx.web to exe_java

--- exe_java.sh
+++ exe_java.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-javac -Xlint --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.base   $1
+javac -Xlint --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.base,javafx.web   $1
 class_file=$(echo $1 | cut -d'.' -f1)
 filename=$1
 echo $filename
 classname=$class_file
 echo $classname
-java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.base  $classname
+java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.base,javafx.web  $classname
