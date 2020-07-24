2f1e5e620e37e7f203d366631067193b17fe6929
Arun Jois <arun.joiss@gmail.com>
2020-07-24 21:04:53 +0530

    Some modifications

--- DirectoryWalk.java
+++ DirectoryWalk.java
@@ -2,12 +2,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class DirectoryWalk {
     public static void main(String args[]) throws IOException{
-        Files.walk(Paths.get(System.getProperty("user.dir")))
-        .filter(Files::isRegularFile)
-        .forEach(System.out::println);
+        try (Stream<Path> walk = Files.walk(Paths.get(System.getProperty("user.dir")))) {
+
+            List<String> result = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+        
+            result.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
