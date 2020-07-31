13fa2560fe9c43b8b918304d549fd9b70ea1ad47
Arun Jois <arun.joiss@gmail.com>
2020-07-31 12:17:47 +0530

    Dummy

--- LeftShift.java
+++ LeftShift.java
@@ -2,7 +2,7 @@ public class LeftShift {
     public static void main(String args[]) {
         int a = 2;
         int b = 1;
-        for( int i =0 ; i < 10 ; i++) {
+        for( int i = 0 ; i < 10 ; i++) {
             a = a << b;
             System.out.println(a);
         }
