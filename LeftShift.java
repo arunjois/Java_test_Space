85e878407f3c5e34dce42b11c3f9ed0357ebcd79
Arun Jois <arun.joiss@gmail.com>
2020-07-31 12:13:48 +0530

    Added LeftShift

---
+++ LeftShift.java
@@ -0,0 +1,10 @@
+public class LeftShift {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 1;
+        for( int i =0 ; i < 10 ; i++) {
+            a = a << b;
+            System.out.println(a);
+        }
+    }
+}
