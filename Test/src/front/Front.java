70e3d9ae3a8002107bd735340b51087ca07fd3be
Arun Jois <arun.joiss@gmail.com>
2020-08-23 16:52:44 +0530

    Clear Everything in front/

--- Test/src/front/Front.java
+++ Test/src/front/Front.java
@@ -1,12 +1,1 @@
-package front;
-import javafx.scene.layout.StackPane;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-public class Front {
-    public BorderPane getFront() {
-        BorderPane sp = new BorderPane();
-        StackPane stp = new StackPane(new Button("HAHA Stackpane"));
-        sp.setCenter(stp);
-        return sp;
-    }
-}
+
