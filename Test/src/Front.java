2c67dbcc5785529b6cf639fc40d030d8888c9ec6
Arun Jois <arun.joiss@gmail.com>
2020-08-24 22:58:51 +0530

    Replicates of two files

---
+++ Test/src/Front.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+public class Front extends Application {
+    public static void main(String args[]) {
+        launch(args);
+    }
+    @Override 
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        VBoxSet vbset = new VBoxSet();
+        //root.setCenter(vbset.getVBox());
+        root.setTop(vbset.getVBox());
+
+
+
+
+        Scene myscene = new  Scene(root);
+        primaryStage.setScene(myscene);
+        primaryStage.setMaximized(true);;
+        primaryStage.show();
+    }
+}
---
+++ Test/src/VBoxSet.java
@@ -0,0 +1,12 @@
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+public class VBoxSet {
+    public VBox vb;
+    public VBoxSet() {
+        this.vb = new VBox(new Button("HAHA"));
+    }
+    public VBox getVBox() {
+        //this.vb.getChildren().addAll(new Button("haha"));
+        return this.vb;
+    }
+}
