cacee811a170cc3404a7a3f3b243e169a1970322
Arun Jois <arun.joiss@gmail.com>
2020-08-25 23:33:43 +0530

    Change in VB Commit

--- Test/src/VBoxSet.java
+++ Test/src/VBoxSet.java
@@ -3,10 +3,10 @@ import javafx.scene.control.Button;
 public class VBoxSet {
     public VBox vb;
     public VBoxSet() {
-        this.vb = new VBox(new Button("HAHA"));
+        this.vb = new VBox(3);
     }
     public VBox getVBox() {
-        //this.vb.getChildren().addAll(new Button("haha"));
+        this.vb.getChildren().addAll(new Button("haha"));
         return this.vb;
     }
 }
